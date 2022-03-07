f846c85ad7625063a8e8fe324d356e8d65f7c083
felopincha <101112338+felopincha@users.noreply.github.com>
2022-03-07 09:38:01 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-test
+reto colektia
---
+++ setup.py
@@ -0,0 +1,6 @@
+setup(
+    name='prueba',
+    version='1',
+    author='Felix Mena',
+    author_email='felomenapincha@gmail.com',
+    description=('un simple proceso ETL Extract-Transform-Load')
